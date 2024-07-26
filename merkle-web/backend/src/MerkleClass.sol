74ff47508d84615be7dc64d03c94b4d7cdf2068f
bitfalt <75431447+bitfalt@users.noreply.github.com>
2024-07-26 01:26:00 -0600

    Modify Smart Contract and add deploy

--- merkle-web/backend/src/MerkleClass.sol
+++ merkle-web/backend/src/MerkleClass.sol
@@ -4,13 +4,26 @@ pragma solidity ^0.8.26;
 import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 
 contract MerkleClass {
-    uint256 public number;
+    bytes32 public immutable merkleRoot;
+    uint256 public immutable amount;
 
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
+    event Claimed(address account, uint256 amount);
+
+    constructor(bytes32 _merkleRoot, uint256 _amount) {
+        merkleRoot = _merkleRoot;
+        amount = _amount;
     }
 
-    function increment() public {
-        number++;
+    function claim(address _account, bytes32[] calldata _merkleProof) public {
+        // Verify merkle proof
+        bytes32 node = keccak256(abi.encodePacked(_account, amount));
+
+        require(
+            MerkleProof.verify(_merkleProof, merkleRoot, node), 
+            "MerkleClass: Invalid proof."
+        );
+
+        emit Claimed(_account, amount);
     }
+
 }
