657b61011998e9f7677f5eb1e22b32d56aa7f71b
bitfalt <75431447+bitfalt@users.noreply.github.com>
2024-08-01 04:48:36 -0600

    Modify project configuration files

--- merkle-web/frontend/astro.config.mjs
+++ merkle-web/frontend/astro.config.mjs
@@ -6,5 +6,6 @@ import db from "@astrojs/db";
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
   integrations: [tailwind(), react(), db()]
 });
--- merkle-web/frontend/src/env.d.ts
+++ merkle-web/frontend/src/env.d.ts
@@ -1,2 +1,24 @@
 /// <reference path="../.astro/db-types.d.ts" />
-/// <reference types="astro/client" />
+interface ImportMetaEnv {
+    // Firebase config
+    readonly FIREBASE_API_KEY: string;
+    readonly FIREBASE_AUTH_DOMAIN: string;
+    readonly FIREBASE_PROJECT_ID: string;
+    readonly FIREBASE_STORAGE_BUCKET: string;
+    readonly FIREBASE_MESSAGING_SENDER_ID: string;
+    readonly FIREBASE_APP_ID: string;
+    readonly FIREBASE_MEASUREMENT_ID: string;
+    // Firebase service account
+    readonly FIREBASE_PRIVATE_KEY_ID: string;
+    readonly FIREBASE_PRIVATE_KEY: string;
+    readonly FIREBASE_CLIENT_EMAIL: string;
+    readonly FIREBASE_CLIENT_ID: string;
+    readonly FIREBASE_AUTH_URI: string;
+    readonly FIREBASE_TOKEN_URI: string;
+    readonly FIREBASE_AUTH_CERT_URL: string;
+    readonly FIREBASE_CLIENT_CERT_URL: string;
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv;
+}
