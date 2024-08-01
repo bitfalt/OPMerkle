710746da68173c2356b8e1ab47fd0277443b12df
bitfalt <75431447+bitfalt@users.noreply.github.com>
2024-08-01 05:09:49 -0600

    Fix AstroDB problems

--- merkle-web/frontend/astro.config.mjs
+++ merkle-web/frontend/astro.config.mjs
@@ -1,13 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-import db from "@astrojs/db";
 
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  integrations: [tailwind(), react(), db()],
+  integrations: [tailwind(), react()],
   adapter: vercel()
 });
